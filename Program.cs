d8d9de6f8347ab8fb6b79730390b60c7b4d1ff12
Anna Beatriz <annab_oliver@hotmail.com>
2022-09-11 16:08:50 -0300

    Finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Cria a variável com os valores e separa os dados utilizando o separador ","
+
+string dados = ("Anna,89,78,Maria,78,Jose,Mateus,8,6,415,230,45.0,Modal GR");
+string[] numeros = dados.Split(",");
+
+//Exibe os dados separados
+
+Console.WriteLine("==== Dados separados ====\n");
+
+foreach (var numero in numeros) {
+
+    System.Console.WriteLine($"Dado: {numero}");
+
+}
